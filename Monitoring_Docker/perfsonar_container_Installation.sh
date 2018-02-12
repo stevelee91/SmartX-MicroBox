4bea4895625a4b8e27bf06ecc6e16b251d544669
OF@TEIN <ahmadrathore@gmail.com>
2018-02-12 10:00:20 +0900

    Update perfsonar_container_Installation.sh

--- Monitoring_Docker/perfsonar_container_Installation.sh
+++ Monitoring_Docker/perfsonar_container_Installation.sh
@@ -1,4 +1,4 @@
-This docker script installs the perfsonar-toolkit and perfsonar-centralmanagement. Installation is based on the latest centos6 build. Before you start you'll need to install docker.
+## This docker script installs the perfsonar-toolkit and perfsonar-centralmanagement. Installation is based on the latest centos6 build. Before you start you'll need to install docker.
 
 In debian this can be done with the following commands:
 
